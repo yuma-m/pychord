cabab79ae0820f5db9db176de05df4dae32737b1
pythagoras_mac <60305361+pythagorasisreal@users.noreply.github.com>
2020-04-12 13:12:32 +1200

    Add SHARPED_SCALE to base directory

--- pychord/constants/__init__.py
+++ pychord/constants/__init__.py
@@ -1,2 +1,2 @@
 from .qualities import QUALITY_DICT
-from .scales import NOTE_VAL_DICT, VAL_NOTE_DICT, SCALE_VAL_DICT
+from .scales import NOTE_VAL_DICT, VAL_NOTE_DICT, SCALE_VAL_DICT, SHARPED_SCALE
