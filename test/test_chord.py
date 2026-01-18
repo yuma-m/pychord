f2a35a61005e6b5541acd8f5fc8c589a5de5e687
Yuma Mihira <yuma-m@users.noreply.github.com>
2026-01-18 20:19:10 +0900

    Use parameterized in tests (#99)
    This PR simplifies test cases by using `parameterized`.

--- test/test_chord.py
+++ test/test_chord.py
@@ -1,84 +1,56 @@
 import unittest
 
+from parameterized import parameterized
+
 from pychord import Chord
 
 
 class TestChordCreations(unittest.TestCase):
 
-    def test_normal_chord(self):
-        c = Chord("C")
-        self.assertEqual(c.root, "C")
-        self.assertEqual(c.quality.quality, "")
+    @parameterized.expand([
+        ("C", "C", ""),
+        ("Am", "A", "m"),
+        ("A-", "A", "-"),
+        ("C69", "C", "69"),
+        ("Bm7-5", "B", "m7-5"),
+        ("Dm7b5", "D", "m7b5"),
+    ])
+    def test_chord_creation(self, chord_str, expected_root, expected_quality):
+        c = Chord(chord_str)
+        self.assertEqual(expected_root, c.root)
+        self.assertEqual(expected_quality, c.quality.quality)
 
-    def test_invalid_chord(self):
-        self.assertRaises(ValueError, Chord, "H")
+    @parameterized.expand([
+        ("H",),
+        ("Csus3",),
+    ])
+    def test_invalid_chord(self, chord_str):
+        self.assertRaises(ValueError, Chord, chord_str)
 
-    def test_minor_chord(self):
-        c = Chord("Am")
-        self.assertEqual(c.root, "A")
-        self.assertEqual(c.quality.quality, "m")
-
-    def test_minor_chord_with_minus(self):
-        c = Chord("A-")
-        self.assertEqual(c.root, "A")
-        self.assertEqual(c.quality.quality, "-")
-
-    def test_69_chord(self):
-        c = Chord("C69")
-        self.assertEqual(c.root, "C")
-        self.assertEqual(c.quality.quality, "69")
-
-    def test_m75_chord(self):
-        c = Chord("Bm7-5")
-        self.assertEqual(c.root, "B")
-        self.assertEqual(c.quality.quality, "m7-5")
-
-    def test_invalid_quality_chord(self):
-        self.assertRaises(ValueError, Chord, "Csus3")
-
-    def test_slash_chord(self):
-        c = Chord("F/G")
-        self.assertEqual(c.root, "F")
-        self.assertEqual(c.quality.quality, "")
-        self.assertEqual(c.on, "G")
-
-    def test_minor_slash_chord(self):
-        c = Chord("Dm/G")
-        self.assertEqual(c.root, "D")
-        self.assertEqual(c.quality.quality, "m")
-        self.assertEqual(c.on, "G")
-
-    def test_minor_7b5_chord(self):
-        c = Chord("Dm7b5")
-        self.assertEqual(c.root, "D")
-        self.assertEqual(c.quality.quality, "m7b5")
+    @parameterized.expand([
+        ("F/G", "F", "", "G"),
+        ("Dm/G", "D", "m", "G"),
+    ])
+    def test_slash_chord(self, chord_str, expected_root, expected_quality, expected_on):
+        c = Chord(chord_str)
+        self.assertEqual(expected_root, c.root)
+        self.assertEqual(expected_quality, c.quality.quality)
+        self.assertEqual(expected_on, c.on)
 
     def test_invalid_slash_chord(self):
         self.assertRaises(ValueError, Chord, "C/H")
 
-    def test_1st_order_inversion(self):
-        c = Chord("C/1")
-        self.assertEqual(c.root, "C")
-        self.assertEqual(c.quality.quality, "")
-        self.assertEqual(c.components(), ["E", "G", "C"])
-
-    def test_2nd_order_inversion(self):
-        c = Chord("C/2")
-        self.assertEqual(c.root, "C")
-        self.assertEqual(c.quality.quality, "")
-        self.assertEqual(c.components(), ["G", "C", "E"])
-
-    def test_inversion_complicated(self):
-        c = Chord("Dm7b5/1")
-        self.assertEqual(c.root, "D")
-        self.assertEqual(c.quality.quality, "m7b5")
-        self.assertEqual(c.components(), ["F", "G#", "C", "D"])
-
-    def test_inversion_with_alternate_bass(self):
-        c = Chord("C/1/F")
-        self.assertEqual(c.root, "C")
-        self.assertEqual(c.quality.quality, "")
-        self.assertEqual(c.components(), ["F", "E", "G", "C"])
+    @parameterized.expand([
+        ("C/1", "C", "", ["E", "G", "C"]),
+        ("C/2", "C", "", ["G", "C", "E"]),
+        ("Dm7b5/1", "D", "m7b5", ["F", "G#", "C", "D"]),
+        ("C/1/F", "C", "", ["F", "E", "G", "C"]),
+    ])
+    def test_inversion(self, chord_str, expected_root, expected_quality, expected_components):
+        c = Chord(chord_str)
+        self.assertEqual(expected_root, c.root)
+        self.assertEqual(expected_quality, c.quality.quality)
+        self.assertEqual(expected_components, c.components())
 
     def test_eq(self):
         c1 = Chord("C")
@@ -103,51 +75,37 @@ class TestChordCreations(unittest.TestCase):
 
 
 class TestChordFromNoteIndex(unittest.TestCase):
-    def test_note_1(self):
-        chord = Chord.from_note_index(note=1, quality="", scale="Cmaj")
-        self.assertEqual(chord, Chord("C"))
 
-    def test_note_2(self):
-        chord = Chord.from_note_index(note=2, quality="m7", scale="F#min")
-        self.assertEqual(chord, Chord("G#m7"))
+    @parameterized.expand([
+        (1, "", "Cmaj", "C"),
+        (2, "m7", "F#min", "G#m7"),
+        (3, "sus2", "Cmin", "Ebsus2"),
+        (7, "7", "Amin", "G7"),
+        (8, "", "Emaj", "E"),
+    ])
+    def test_from_note_index(self, note, quality, scale, expected_chord):
+        chord = Chord.from_note_index(note=note, quality=quality, scale=scale)
+        self.assertEqual(Chord(expected_chord), chord)
 
-    def test_note_3(self):
-        chord = Chord.from_note_index(note=3, quality="sus2", scale="Cmin")
-        self.assertEqual(chord, Chord("Ebsus2"))
-
-    def test_note_7(self):
-        chord = Chord.from_note_index(note=7, quality="7", scale="Amin")
-        self.assertEqual(chord, Chord("G7"))
-
-    def test_note_8(self):
-        chord = Chord.from_note_index(note=8, quality="", scale="Emaj")
-        self.assertEqual(chord, Chord("E"))
-
-    def test_note_0(self):
+    @parameterized.expand([
+        (0, "", "Cmaj"),
+        (9, "", "Fmaj"),
+    ])
+    def test_invalid_note_index(self, note, quality, scale):
         with self.assertRaises(ValueError):
-            Chord.from_note_index(note=0, quality="", scale="Cmaj")
+            Chord.from_note_index(note=note, quality=quality, scale=scale)
 
-    def test_note_9(self):
-        with self.assertRaises(ValueError):
-            Chord.from_note_index(note=9, quality="", scale="Fmaj")
+    @parameterized.expand([
+        (1, "", True, "Dmaj", "D"),
+        (2, "7", True, "BLoc", "Cmaj7"),
+        (3, "m", True, "G#Mix", "Cdim"),
+        (4, "-", True, "AbDor", "C#"),
+    ])
+    def test_diatonic_from_note_index(self, note, quality, diatonic, scale, expected_chord):
+        chord = Chord.from_note_index(note=note, quality=quality, diatonic=diatonic, scale=scale)
+        self.assertEqual(Chord(expected_chord), chord)
 
-    def test_diatonic_note_1(self):
-        chord = Chord.from_note_index(note=1, quality="", diatonic=True, scale="Dmaj")
-        self.assertEqual(chord, Chord("D"))
-
-    def test_diatonic_note_2_mode(self):
-        chord = Chord.from_note_index(note=2, quality="7", diatonic=True, scale="BLoc")
-        self.assertEqual(chord, Chord("Cmaj7"))
-
-    def test_diatonic_note_3_mode(self):
-        chord = Chord.from_note_index(note=3, quality="m", diatonic=True, scale="G#Mix")
-        self.assertEqual(chord, Chord("Cdim"))
-
-    def test_diatonic_note_4_mode(self):
-        chord = Chord.from_note_index(note=4, quality="-", diatonic=True, scale="AbDor")
-        self.assertEqual(chord, Chord("C#"))
-
-    def test_diatonic_note_nongeneric(self):
+    def test_diatonic_note_non_generic(self):
         with self.assertRaises(NotImplementedError):
             Chord.from_note_index(note=5, quality="sus", diatonic=True, scale="Fmaj")
 
